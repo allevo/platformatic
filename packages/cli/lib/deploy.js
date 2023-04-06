ee30ad9caf200c7cd00f362dc3c9a14f3a04fbce
Ivan Tymoshenko <bugagashenkj@gmail.com>
2023-04-06 20:20:31 +0200

    fix: change default deploy service url

--- packages/cli/lib/deploy.js
+++ packages/cli/lib/deploy.js
@@ -8,7 +8,7 @@ import inquirer from 'inquirer'
 import parseArgs from 'minimist'
 import deployClient from '@platformatic/deploy-client'
 
-export const DEPLOY_SERVICE_HOST = 'https://plt-development-deploy-service.fly.dev'
+export const DEPLOY_SERVICE_HOST = 'https://plt-production-deploy-service.fly.dev'
 
 const WORKSPACE_TYPES = ['static', 'dynamic']
 const UUID_REGEX = /^[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}$/i
